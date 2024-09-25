254b4d5152f8cfe5123d6cba56dd297caaed2151
spader7 <a24gusce@student.his.se>
2024-09-25 11:49:24 +0200

    just added classes

---
+++ Entity.cs
@@ -0,0 +1,26 @@
+namespace Platformer;
+
+class Entity
+{
+
+}
+class Platform : Entity
+{
+
+}
+class Background : Entity
+{
+
+}
+class Key : Entity
+{
+
+}
+class Door : Entity
+{
+
+}
+class Hero : Entity
+{
+
+}
