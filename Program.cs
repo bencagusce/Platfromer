bc28760565f95da326015d6974c016ee19ddf72e
Benjamin Calmerman <a24benca@student.his.se>
2024-09-25 16:32:35 +0200

    syncing Program

--- Program.cs
+++ Program.cs
@@ -15,13 +15,34 @@ class platformer
             
             // TODO: Initialize
             Clock clock = new Clock();
+            Scene scene = new Scene();
+
+            scene.Spawn(new Platform {
+                Position = new Vector2f(54, 270)});
+
+            for (int i = 0; i < 10; i++)
+            {
+                scene.Spawn(new Platform {
+                    Position = new Vector2f(18 + i * 18, 288)
+                });
+            }
+            
+            window.SetView(new View(
+                new Vector2f(200, 150),
+                new Vector2f(400, 300)
+            ));
+            
             while (window.IsOpen)
             {
                 window.DispatchEvents();
                 float deltaTime = clock.Restart().AsSeconds();
                 // TODO: Updates
+                scene.UpdateAll(deltaTime);
+                
                 window.Clear();
                 // TODO: Drawing
+                scene.RenderAll(window);
+                
                 window.Display();
             }
         }
